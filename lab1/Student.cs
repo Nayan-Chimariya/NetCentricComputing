9d8e51b9a96837cca2b090bd8ef4c011a126d6df
Sleep deprived <cnayan789@gmail.com>
2024-03-20 09:28:04 +0545

    modification required

---
+++ lab1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace lab1
+{
+	public class Student
+	{
+		public string name, ID, address;
+
+		public void GetData()
+		{
+			Console.WriteLine("Enter name: ");
+            Console.WriteLine("Enter ID: ");
+            Console.WriteLine("Enter address: ");
+        }
+
+		public void DisplayData()
+		{
+			Console.WriteLine("{0}{1}{2}", name, ID, address); Console.WriteLine("Enter name: ");
+            Console.ReadLine();
+        }
+            
+		public void Read(string path)
+		{
+			FileStream file = new(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+		}
+	}
+}
+
+
+           
+
+
+
